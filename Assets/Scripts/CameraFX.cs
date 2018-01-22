b409dc371a4043bf67c50bf9ee2e747fb7addd37
DeviTobi <DeviTobi@users.noreply.github.com>
2018-01-22 16:21:32 +0100

    Fade Effect WIP

---
+++ Assets/Scripts/CameraFX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFX : MonoBehaviour {
+
+    private static SpriteRenderer screenFade;
+    private static float alpha;
+
+	private void Start ()
+    {
+        screenFade = transform.GetChild(0).GetComponent<SpriteRenderer>();
+	}
+
+    private void Update()
+    {
+        alpha = Mathf.PingPong(Time.time, 1);
+        StartFadeProcess(1);
+
+        Debug.Log(alpha);
+    }
+
+    public static void StartFadeProcess(float duration)
+    {
+        screenFade.color = new Color(screenFade.color.r, screenFade.color.g, screenFade.color.b, alpha);
+    }
+}
