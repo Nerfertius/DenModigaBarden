e6ff887e5ba6329f9fabd543616b34f77077c1aa
Nerfertius <nerfertius@gmail.com>
2018-01-23 12:02:40 +0100

    Player actions and conditions
    !WIP!
    Started moving player movement code from placeholder to actions and conditions. WalkState, AirState & ClimbState

---
+++ Assets/Scripts/StateMachine/Player/Actions/PlayerWalkAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Action/Player/PlayerWalkAction")]
+public class PlayerWalkAction : StateAction {
+
+    public override void FixedAct(StateController controller)
+    {
+		PlayerData data = (PlayerData)controller.data;
+		data.moveHorizontal = Input.GetAxis("Horizontal");
+		data.movement = new Vector2(data.moveHorizontal, 0);
+		data.body.AddForce(data.movement * data.speedMod);
+
+		if (Mathf.Abs(data.body.velocity.x) > data.maxSpeed)
+		{
+			if (data.body.velocity.x > 0)
+			{
+				data.body.velocity = new Vector2(data.maxSpeed, data.body.velocity.y);
+			}
+			else if (data.body.velocity.x < 0)
+			{
+				data.body.velocity = new Vector2(data.maxSpeed * -1, data.body.velocity.y);
+			}
+		}
+	}
+
+	public override void Act(StateController controller)
+	{
+		PlayerData data = (PlayerData)controller.data;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			data.body.AddForce(new Vector2(0, data.jumpPower));
+			data.grounded = false;
+		}
+	}
+}
---
+++ Assets/Scripts/StateMachine/Player/Conditions/PlayerClimbOn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Condition/Player/PlayerClimbOn")]
+public class PlayerClimbOn : Condition
+{
+	public override bool? CheckTriggerEnter(StateController controller, Collider2D other)
+	{
+		if (other.CompareTag("Ladder"))
+		{
+			PlayerData data = (PlayerData)controller.data;
+			data.ladderBottom = other.transform.parent.GetChild(0).gameObject;
+			data.ladderTop = other.transform.parent.GetChild(1).gameObject;
+			return true;
+		}
+		return false;
+	}
+
+	public override bool? CheckTriggerStay(StateController controller, Collider2D other)
+	{
+		if (other.CompareTag("Ladder"))
+		{
+			PlayerData data = (PlayerData)controller.data;
+
+			//Bottom
+			if (Input.GetKey(KeyCode.W) && data.transform.position.y < data.ladderBottom.transform.position.y)
+			{
+				data.body.isKinematic = true;
+				data.body.velocity = Vector2.zero;
+				data.transform.position = new Vector2(other.transform.position.x, other.transform.position.y);
+				return true;
+			}
+			//Top
+			else if (Input.GetKey(KeyCode.S) && data.transform.position.y > data.ladderTop.transform.position.y)
+			{
+				data.body.isKinematic = true;
+				data.body.velocity = Vector2.zero;
+				data.transform.position = new Vector2(other.transform.position.x, other.transform.position.y + 0.5f);
+				return true;
+			}
+			//Between
+			else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W)) && data.transform.position.y < data.ladderTop.transform.position.y && data.transform.position.y > data.ladderBottom.transform.position.y)
+			{
+				data.body.isKinematic = true;
+				data.body.velocity = Vector2.zero;
+				data.transform.position = new Vector2(other.transform.position.x, data.transform.position.y);
+				return true;
+			}
+		}
+		return false;
+	}
+}
