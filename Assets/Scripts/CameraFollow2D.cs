3e2bde0d404a0056058594fc271ab08b47b555b3
DeviTobi <DeviTobi@users.noreply.github.com>
2018-01-24 12:52:17 +0100

    StateController Update + Metroidvania Camera
    StateController:
    Multiple Entry & Exit actions are now possible
    
    Metroidvania Camera:
    -Camera Follow script
    -Camera bound to room space
    
    CameraFX:
    - Optimization
    
    Room Transitor:
    - WIP

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraFollow2D : MonoBehaviour {
+
+	public Transform target;
+    public bool hideCursor;
+    [Range(1, 10)] public int followSpeed;
+    [Range(1, 10)] public int transitionSpeed;
+
+    Vector2 topLeft, bottomRight;
+    bool transitioning = false;
+    float posX, posY;
+
+    void Start(){
+    	Cursor.visible = !hideCursor;
+    }
+
+	void LateUpdate ()
+	{
+		posX = Mathf.Clamp (target.transform.position.x, topLeft.x, bottomRight.x);
+		posY = Mathf.Clamp (target.transform.position.y, bottomRight.y, topLeft.y);
+
+		if (!transitioning) {
+			transform.position = Vector3.MoveTowards (transform.position, new Vector3 (posX, posY, transform.position.z), 0.64f * followSpeed);
+			//transform.position = new Vector3 (posX, posY, transform.position.z);
+		} else {
+			RoomTransition ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.LeftAlt)) {
+			Cursor.visible = !Cursor.visible;
+		}
+	}
+
+	void RoomTransition ()
+	{
+		if (transform.position.x < topLeft.x || transform.position.x > bottomRight.x || transform.position.y > topLeft.y || transform.position.y < bottomRight.y) {
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3 (posX, posY, transform.position.z), 15 * transitionSpeed * Time.deltaTime);
+		} else {
+			transitioning = false;
+		}
+	}
+
+    public void SetMapBoundary(Vector2 topLeft, Vector2 bottomRight)
+    {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+        transitioning = true;
+    }
+}
---
+++ Assets/Scripts/RoomTransitor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransitor : MonoBehaviour {
+	PlayerData data;
+    //EnemyManager em;
+    
+    public Vector2 targetPosition;
+
+	void Start (){
+	//	em = transform.parent.GetComponent<EnemyManager>();
+	}
+
+    void Update()
+    {
+        Debug.DrawLine(transform.position, (Vector2)transform.position + targetPosition,  Color.red);
+    }
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			if (data == null) {
+				data = (PlayerData)other.GetComponent<StateController>().data;
+			}
+
+			if (!data.inTransit) {
+				data.inTransit = true;
+                data.targetPos = (Vector2)transform.position + targetPosition;
+				StartCoroutine("PlayerReachedNextRoom");
+			}
+		}
+	}
+
+	IEnumerator PlayerReachedNextRoom ()
+	{
+		while (data.inTransit) {
+			yield return new WaitForSeconds (0.1f);
+		}
+
+		//em.DeactivateAllEnemies();	
+		yield break;
+	}
+}
