b0a8b5f287b546ff23edeadb808a16be8c05dcde
DeviTobi <DeviTobi@users.noreply.github.com>
2018-01-25 14:10:57 +0100

    Updated Patrol

--- Assets/Scripts/StateMachine/Enemies/Actions/PatrolAction.cs
+++ Assets/Scripts/StateMachine/Enemies/Actions/PatrolAction.cs
@@ -31,7 +31,8 @@ public class PatrolAction : StateAction {
 
     private bool NextToWall(StateController controller)
     {
-        RaycastHit2D hit = Physics2D.Raycast(controller.transform.position, controller.data.currentDirection, 0.525f, mask);
+        float offset = controller.coll.bounds.size.x * 0.5f;
+        RaycastHit2D hit = Physics2D.Raycast(controller.transform.position, controller.data.currentDirection, offset, mask);
 
         return (hit);
     }
@@ -41,12 +42,12 @@ public class PatrolAction : StateAction {
         Vector2 offset;
 
         if (controller.sprRend.flipX) {
-            offset = new Vector2(controller.coll.bounds.size.x, 0);
+            offset = new Vector2(controller.coll.bounds.size.x * 0.5f, 0);
         } else {
-            offset = new Vector2(-controller.coll.bounds.size.x, 0);
+            offset = new Vector2(-controller.coll.bounds.size.x * 0.5f, 0);
         }
 
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)controller.transform.position + offset, Vector2.down, 5f, mask);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)controller.transform.position + offset, Vector2.down, 1f, mask);
         Debug.DrawRay(controller.transform.position + (Vector3)offset, Vector3.down, Color.red, 0.1f);
         
         return (hit.collider == null);
