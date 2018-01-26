694ab3c55f1458a648059b77c4a6236c0783435e
DeviTobi <DeviTobi@users.noreply.github.com>
2018-01-26 12:12:24 +0100

    General Condition
    Falling added

--- Assets/Scripts/StateMachine/Enemies/Conditions/PlayerSighted.cs
+++ Assets/Scripts/StateMachine/Enemies/Conditions/PlayerSighted.cs
@@ -11,7 +11,17 @@ public class PlayerSighted : Condition {
         if (eData.player == null)
             return false;
 
+        if (eData.currentDirection.x == 1 && eData.player.position.x < controller.transform.position.x)
+        {
+            return false;
+        }
+        else if (eData.currentDirection.x == -1 && eData.player.position.x > controller.transform.position.x)
+        {
+            return false;
+        }
+
         RaycastHit2D hit = Physics2D.Linecast(controller.transform.position, eData.player.position);
+
 
         return (hit.collider.tag == "Player");
     }
---
+++ Assets/Scripts/StateMachine/General/Conditions/Falling.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Condition/General/Falling")]
+
+public class Falling : Condition
+{
+    public LayerMask mask;
+
+    public override bool? CheckCondition(StateController controller)
+    {
+        return !(Physics2D.OverlapBox((Vector2)controller.transform.position - new Vector2(0, controller.coll.bounds.size.y / 10), (Vector2)controller.coll.bounds.size - new Vector2(0.1f,0), 0, mask));
+    }
+}
