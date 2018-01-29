3c0bb071b1e55b3116aa524b588340385a3ca87c
DeviTobi <DeviTobi@users.noreply.github.com>
2018-01-29 12:02:40 +0100

    More bug fixes

--- Assets/Scripts/StateMachine/Enemies/Conditions/PlayerSighted.cs
+++ Assets/Scripts/StateMachine/Enemies/Conditions/PlayerSighted.cs
@@ -21,12 +21,15 @@ public class PlayerSighted : Condition {
         else if (eData.currentDirection.x == -1 && eData.player.position.x > controller.transform.position.x)
         {
             return false;
+        } else if (controller.transform.position.y + 2 < eData.player.position.y)
+        {
+            return false;
         }
 
         RaycastHit2D hit;
         if (controller.transform.position.y < eData.player.position.y - 0.3f)
         {
-            hit = Physics2D.Linecast(controller.transform.position, eData.player.position, mask, 0);
+            hit = Physics2D.Linecast(controller.transform.position, eData.player.position, mask);
         } else
         {
             hit = Physics2D.Linecast(controller.transform.position, eData.player.position, mask, -1);
