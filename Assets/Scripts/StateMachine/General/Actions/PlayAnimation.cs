4993f166a160bd3c427d515e1fec0be3d9825f34
AntOnOakMountain <AntOnOakMountain@gmail.com>
2018-01-30 11:45:59 +0100

    Gargoyle

---
+++ Assets/Scripts/StateMachine/General/Actions/PlayAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "StateMachine/Action/General/PlayAnimation")]
+public class PlayAnimation : StateAction {
+    public string animationName;
+
+    public override void ActOnce(StateController controller) {
+        controller.anim.Play(animationName);
+    }
+
+}
---
+++ Assets/Scripts/StateMachine/General/Conditions/CollidedWith.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Condition/General/CollidedWith")]
+public class CollidedWith : Condition {
+
+    public LayerMask mask;
+
+    public override bool? CheckCollisionEnter(StateController controller, Collision2D other)
+    {
+        return mask == (mask | (1 << other.gameObject.layer)); //supposedly checks if any layer in mask is layer
+    }
+
+}
