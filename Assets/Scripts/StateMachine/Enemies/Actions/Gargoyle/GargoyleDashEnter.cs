d17ca0db4132c6c9dc3da577945e5845e270f158
AntOnOakMountain <AntOnOakMountain@gmail.com>
2018-01-30 14:28:24 +0100

    gargoyle mostly done

---
+++ Assets/Scripts/StateMachine/Enemies/Actions/Gargoyle/GargoyleDashEnter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Action/Enemy/Gargoyle/GargoyleDashEnter")]
+public class GargoyleDashEnter : StateAction {
+
+    public override void ActOnce(StateController controller) {
+        GargoyleData data = (GargoyleData)controller.data;
+
+        data.dashCollider.enabled = true;
+        data.idleCollider.enabled = false;
+    }
+}
---
+++ Assets/Scripts/StateMachine/Enemies/Actions/Gargoyle/GargoyleIdleEnter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Action/Enemy/Gargoyle/GargoyleIdleEnter")]
+public class GargoyleIdleEnter : StateAction {
+
+    public override void ActOnce(StateController controller) {
+        GargoyleData data = (GargoyleData)controller.data;
+
+        data.dashCollider.enabled = false;
+        data.idleCollider.enabled = true;
+    }
+
+}
