041be356e11ee905d336c6f7b3938c4c06026bca
Nerfertius <nerfertius@gmail.com>
2018-02-01 14:32:19 +0100

    House prefabs added

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class House : MonoBehaviour {
+public class House : MonoBehaviour
+{
+    public Transform houseLocation;
+    
+    private GameObject player;
+    private bool playerNear;
+    
+    void Update ()
+    {
+        if (Input.GetButtonDown("EnterHouse") && playerNear)
+        {
+            //StartCoroutine(EnterHouse());
+        }
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //IEnumerator EnterHouse()
+    //{
+    //    // FadeIn & FadeOut doesn't work
+
+    //    //CameraFX.FadeOut();
+    //    //player.transform.position = houseLocation.position;
+    //    //yield return new WaitForSeconds(2f);
+    //    //CameraFX.FadeIn();
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerNear = true;
+            player = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerNear = false;
+            player = null;
+        }
+    }
 }
