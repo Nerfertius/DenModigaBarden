581537968093a511e3d72948e9f0c9db235395dd
rickfo97 <rickfo_97@hotmail.com>
2018-02-08 15:39:40 +0100

    TransitionState start

---
+++ Assets/Scripts/GameManager/States/TransitionState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionState : GameState {
+
+    public TransitionState(GameManager gm)
+    {
+        this.gm = gm;
+    }
+
+    public override void enter()
+    {
+        gm.hideCanvas("PlayCanvas");
+    }
+
+    public override void exit()
+    {
+        gm.showCanvas("PlayCanvas");
+    }
+
+    public override void update()
+    {
+
+    }
+}
