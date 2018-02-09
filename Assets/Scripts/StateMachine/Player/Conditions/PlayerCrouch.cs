2347e3bd7709223d755804af94d5d958dc783f43
Nerfertius <nerfertius@gmail.com>
2018-02-09 15:15:11 +0100

    some fix?

--- Assets/Scripts/StateMachine/Player/Conditions/PlayerCrouch.cs
+++ Assets/Scripts/StateMachine/Player/Conditions/PlayerCrouch.cs
@@ -7,7 +7,7 @@ public class PlayerCrouch : Condition
 {
     public override bool? CheckCondition(StateController controller)
     {
-        if (Input.GetAxisRaw("Crouch") == 1)
+        if (Input.GetAxisRaw("Vertical") == -1)
         {
             return true;
         }
