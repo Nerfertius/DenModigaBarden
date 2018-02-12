be1a4dfbbe9a7e701acfcbd20618e06dbb77e25b
AntOnOakMountain <AntOnOakMountain@gmail.com>
2018-02-12 16:12:48 +0100

    Player merged channel into normal

---
+++ Assets/Scripts/StateMachine/Player/Actions/PlayerStopPlayingMelody.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Action/Player/PlayerStopPlayingMelody")]
+public class PlayerStopPlayingMelody : StateAction {
+   public override void ActOnce(StateController controller) {
+       PlayerData data = (PlayerData)controller.data;
+       data.melodyData.currentMelody = null;
+       data.melodyData.playingFlute = false;
+       data.melodyData.MelodyRange.enabled = false;
+       data.melodyData.PlayedNotes.Clear();
+       controller.anim.SetBool("Channeling", false);
+   }
+}
