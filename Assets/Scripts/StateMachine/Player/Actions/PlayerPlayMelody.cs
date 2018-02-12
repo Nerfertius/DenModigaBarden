b3234c0bdc8c00e1e583a10c0133609bf9f0b842
AntOnOakMountain <AntOnOakMountain@gmail.com>
2018-02-12 14:51:12 +0100

    Revert "Merged channel states int normal states"
    This reverts commit b6c199d650646e6b5824cf40549fef8cc27c1af3.

--- Assets/Scripts/StateMachine/Player/Actions/PlayerPlayMelody.cs
+++ Assets/Scripts/StateMachine/Player/Actions/PlayerPlayMelody.cs
@@ -9,7 +9,12 @@ public class PlayerPlayMelody : StateAction {
     public override void Act(StateController controller) {
         PlayerData data = (PlayerData)controller.data;
         PlayerData.MelodyData mData = data.melodyData;
+        
+        foreach (Note note in mData.Notes) {
+            if (Input.GetButtonDown(note.Button)) {
+                mData.PlayedNotes.AddLast(note);
 
+<<<<<<< HEAD
         foreach (Note note in mData.Notes) {
             if (Input.GetButtonDown(note.Button)) {
                 mData.PlayedNotes.AddLast(note);
@@ -83,5 +88,51 @@ public class PlayerPlayMelody : StateAction {
                 }
             }
         }
+=======
+                if (Input.GetButton("HighPitch"))
+                {
+                    data.audioSource.pitch = mData.highPitchValue;
+                }
+                else if (Input.GetButton("LowPitch"))
+                {
+                    data.audioSource.pitch = mData.lowPitchValue;
+                }
+                else
+                {
+                    data.audioSource.pitch = mData.standardPitchValue;
+                }
+                data.PlaySound(note.audio);
+
+                ParticleSystem m_fx = data.noteFX;
+                ParticleSystem.TextureSheetAnimationModule m_anim = m_fx.textureSheetAnimation;
+                m_anim.rowIndex = note.FXRowNumber;
+                Instantiate(m_fx, new Vector2(data.transform.position.x, data.spriteRenderer.bounds.max.y), Quaternion.Euler(data.noteFX.transform.rotation.eulerAngles));
+                m_fx.GetComponent<FXdestroyer>().hasPlayed = true;
+            }
+        }
+
+        if (Input.GetButtonUp("PlayMelody"))
+        {
+            foreach (Melody melody in mData.melodies)
+            {
+                if (melody.CheckMelody(mData.PlayedNotes))
+                {
+                    mData.currentMelody = melody.melodyID;
+                    mData.PlayedNotes.Clear();
+                    mData.MelodyRange.enabled = true;
+                    controller.anim.SetBool("Channeling", true);
+                    break;
+                } else
+                {
+                    mData.currentMelody = null;
+                }
+            }
+            mData.PlayedNotes.Clear();
+        }
+
+        while (mData.PlayedNotes.Count > mData.MaxSavedNotes) {
+            mData.PlayedNotes.RemoveFirst();
+        }
+>>>>>>> parent of b6c199d... Merged channel states int normal states
     }
 }
