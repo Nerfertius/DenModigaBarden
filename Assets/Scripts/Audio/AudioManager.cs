9976ac100e08d25eacac50ae5fb49c169da6645f
AntOnOakMountain <AntOnOakMountain@gmail.com>
2018-02-19 13:26:14 +0100

    AudioManagerStart

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public GameObject audioSourceObject;
+    public int numberOfpooledObjects;
+
+    private static AudioManager instance;
+
+    private ComponentPool<AudioSource> audioSourcePool;
+    private LinkedList<AudioSource> activeAudioSources;
+
+    private AudioSource BGMSource1;
+    private AudioSource BGMSource2;
+    private bool currentBGMSource1;
+
+    private AudioSource[] noteSources;
+    private int activeNote;
+
+    
+    private void Start()
+    {
+        if(instance != null) {
+            instance = this;
+        }
+        else {
+            Debug.LogError("There should only be one audioManager " + this.gameObject);
+        }
+
+        audioSourcePool = new ComponentPool<AudioSource>(audioSourceObject, numberOfpooledObjects);
+        activeAudioSources = new LinkedList<AudioSource>();
+        BGMSource1 = new AudioSource();
+        BGMSource2 = new AudioSource();
+    }
+
+    private void Update() {
+        foreach(AudioSource s in activeAudioSources) {
+            if(s.time >= s.clip.length) {
+                audioSourcePool.Free(s);
+                activeAudioSources.Remove(s);
+            }
+        }
+    }
+
+    public void PlayOneShot(AudioClip ac, float volume) {
+        AudioSource source = audioSourcePool.Get();
+        resetAudioSource(source);
+        source.clip = ac;
+        source.Play();
+        activeAudioSources.AddLast(source);
+    }
+
+    public void PlayOneShot(AudioClip ac) {
+        PlayOneShot(ac, 1);
+    }
+
+    private void resetAudioSource(AudioSource source) {
+        source.volume = 1;
+        source.loop = false;
+    }
+
+
+    public void ChangeBGM(AudioClip music)
+    {
+        AudioSource current;
+        AudioSource next;
+        if (currentBGMSource1) {
+            current = BGMSource1;
+            next = BGMSource2;
+            currentBGMSource1 = false;
+        }
+        else {
+            current = BGMSource2;
+            next = BGMSource1;
+            currentBGMSource1 = true;
+        }
+
+        IEnumerator fadeOut = AudioFade(current, current.volume, 0, 0.5f);
+        StartCoroutine(fadeOut);
+
+        next.clip = music;
+        IEnumerator fadeIn = AudioFade(next, 0, 1, 0.5f);
+        StartCoroutine(fadeIn);
+    }
+
+    public static IEnumerator AudioFade(AudioSource audioSource, float startVolume, float endVolume, float duration) {
+        audioSource.volume = startVolume;
+        Timer timer = new Timer(duration);
+        float volumeDiff = endVolume - startVolume;
+        timer.Start();
+        while (!timer.IsDone()) {
+            audioSource.volume = startVolume + volumeDiff * timer.TimePercentagePassed();
+            yield return new WaitForEndOfFrame();
+        }
+        audioSource.volume = endVolume;
+    }
+}
---
+++ Assets/Scripts/General/ComponentPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentPool<T> where T : Behaviour {
+    private GameObject pooledObject;
+    private int numberOfObjects;
+
+    private List<T> objects;
+    private LinkedList<int> available;
+
+    public ComponentPool(GameObject pooledObject, int numberOfObjects)  {
+        if (numberOfObjects < 0)
+            Debug.LogError(this + " pool cant have negative size");
+        if(pooledObject == null)
+            Debug.LogError(this + " pooledObject cant be null");
+        else if (pooledObject.GetComponent<T>() != null)
+            Debug.LogError(this + " pooledObject must ");
+
+        available = new LinkedList<int>();
+        objects = new List<T>(numberOfObjects);
+        for(int i = 0; i < numberOfObjects; i++) {
+            objects.Add(GameObject.Instantiate(pooledObject).GetComponent<T>());
+            objects[i].enabled = false;
+            available.AddLast(i);
+        }
+    }
+
+    public T Get() {
+        if(available.Count > 0) {
+            T ret = objects[available.Last.Value];
+            ret.enabled = true;
+            available.RemoveLast();
+            return ret;
+        }
+        else {
+            T ret = GameObject.Instantiate(pooledObject.GetComponent<T>());
+            objects.Add(ret);
+            return ret;
+        }
+    }
+
+    public void Free(T obj) {
+        int index = objects.IndexOf(obj);
+        if(index == -1) {
+            Debug.LogError(obj + " object is not part of the pool");
+        }
+        else {
+            //UnityEditor.PrefabUtility.ResetToPrefabState(obj);
+            obj.enabled = false;
+            available.AddLast(index);
+        }
+    }
+}
