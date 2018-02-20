269be211c8b0e7087e08a210fc36ccc36fb226e5
AntOnOakMountain <AntOnOakMountain@gmail.com>
2018-02-20 10:40:26 +0100

    Some enemy soundEffects

---
+++ Assets/Scripts/StateMachine/General/Actions/PlayRandomAudioClipOneShot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Action/General/PlayRandomAudioClipOneShot")]
+public class PlayRandomAudioClipOneShot : StateAction {
+
+    public AudioClip[] clips;
+
+    public override void ActOnce(StateController controller) {
+        AudioManager.Instance.PlayOneShot(clips[Random.Range(0, clips.Length - 1)]);
+    }
+}
