efcebb15c974f6057dc23c43f8ac38b6816da74d
hjalmar4 <36536557+hjalmar4@users.noreply.github.com>
2018-02-28 12:20:23 +0100

    Barnkonversation

---
+++ Assets/Hatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hatch : MonoBehaviour
+{
+    private bool opened;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && Input.GetButtonDown("Interact") && !opened)
+        {
+            Open();
+        }
+    }
+
+    void Open()
+    {
+        GetComponent<Animator>().SetTrigger("Open");
+        opened = true;
+    }
+}
