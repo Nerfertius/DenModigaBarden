ea070ce3f9d533e29c00d08282451cf579f87e3f
Nerfertius <nerfertius@gmail.com>
2018-03-02 11:53:25 +0100

    Sleep & key sprite, fixed edge walk-bug

--- Assets/Scripts/StateMachine/PlayerData.cs
+++ Assets/Scripts/StateMachine/PlayerData.cs
@@ -234,7 +234,7 @@ public class PlayerData : Data
 		body = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
         noteAnim = noteFX.textureSheetAnimation;
-        groundCheckRadius = 0.1f;
+        groundCheckRadius = 0.2f;
 
         climbFixLayer = LayerMask.NameToLayer("Blockable");
         playerLayer = LayerMask.NameToLayer("Player");
