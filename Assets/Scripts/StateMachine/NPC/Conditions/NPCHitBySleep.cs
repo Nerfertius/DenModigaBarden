f444afe29d93ab30bebd2812175fa130dca359a6
Nerfertius <nerfertius@gmail.com>
2018-03-05 10:54:49 +0100

    NPC Sleep state

---
+++ Assets/Scripts/StateMachine/NPC/Conditions/NPCHitBySleep.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Condition/NPC/NPCHitBySleep")]
+public class NPCHitBySleep : Condition
+{
+    public override bool? CheckTriggerEnter(StateController controller, Collider2D other)
+    {
+        if (controller.gameObject.GetComponent<Animator>().HasState(0, Animator.StringToHash("Sleep")))
+        {
+            if (other.CompareTag("PlayerProjectile") && other.GetComponent<MelodyProjectile>().melodyID == Melody.MelodyID.SleepMelody)
+            {
+                return true;
+            }
+        }
+        
+        return null;
+    }
+}
