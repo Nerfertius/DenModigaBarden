86a45f4f7d14b4844d415e57b1c9a4bcf8c8b928
hjalmar4 <36536557+hjalmar4@users.noreply.github.com>
2018-03-16 13:04:58 +0100

    DragonRoom

---
+++ Assets/DragonCinematic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonCinematic : MonoBehaviour {
+
+    public Animator anim;
+
+    private void Start()
+    {
+
+    }
+}
